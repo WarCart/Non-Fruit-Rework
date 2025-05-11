c5a866e9fc8c1b15e4f7ff9fffff0c99052fc7df
Warcar <@imasanov2007@gmail,com>
2025-05-11 14:33:22 +0300

    Minor changes

--- src/main/java/net/warcar/non_fruit_rework/challenges/rushes/BaroqueWorksHardChallenge.java
+++ src/main/java/net/warcar/non_fruit_rework/challenges/rushes/BaroqueWorksHardChallenge.java
@@ -1,8 +1,6 @@
 package net.warcar.non_fruit_rework.challenges.rushes;
 
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
 import net.warcar.non_fruit_rework.NonFruitReworkMod;
 import net.warcar.non_fruit_rework.helpers.LangHelper;
 import xyz.pixelatedw.mineminenomi.api.challenges.ArenaStyle;
@@ -11,7 +9,6 @@ import xyz.pixelatedw.mineminenomi.api.challenges.ChallengeCore;
 import xyz.pixelatedw.mineminenomi.api.challenges.ChallengeDifficulty;
 import xyz.pixelatedw.mineminenomi.challenges.arenas.AlabastaDesertSimpleArena;
 import xyz.pixelatedw.mineminenomi.challenges.baroqueworks.Mr0Challenge;
-import xyz.pixelatedw.mineminenomi.init.ModEntities;
 import xyz.pixelatedw.mineminenomi.init.ModNPCGroups;
 
 public class BaroqueWorksHardChallenge extends Challenge {
@@ -19,21 +16,9 @@ public class BaroqueWorksHardChallenge extends Challenge {
     public static final ResourceLocation REWARD = new ResourceLocation(NonFruitReworkMod.MOD_ID, "rewards/baroque_works");
     public static final ChallengeCore<BaroqueWorksHardChallenge> INSTANCE = new ChallengeCore.Builder("baroque_works_hard", TITLE, BaroqueWorksChallenge.OBJECTIVE, ModNPCGroups.BAROQUE_WORKS.getName(), BaroqueWorksHardChallenge::new)
             .setDifficulty(ChallengeDifficulty.HARD).setDifficultyStars(10).addArena(ArenaStyle.SIMPLE, AlabastaDesertSimpleArena.INSTANCE, AlabastaDesertSimpleArena::getChallengerSpawnPos, BaroqueWorksChallenge::getEnemySpawnPos)
-            .setEnemySpawns(BaroqueWorksChallenge::setEnemySpawns).setTargetShowcase(Mr0Challenge::createMr0Showcase, BaroqueWorksChallenge::createMr1Showcase, BaroqueWorksChallenge::createMr3Showcase, BaroqueWorksChallenge::createMr4Showcase).setTimeLimit(30).setRewards(REWARD).build();
-
-    private static LivingEntity createMr4Showcase(World world) {
-        return ModEntities.MR4.get().create(world);
-    }
-
-    private static LivingEntity createMr3Showcase(World world) {
-        return ModEntities.MR3.get().create(world);
-    }
+            .setEnemySpawns(BaroqueWorksChallenge::setEnemySpawns).setTargetShowcase(Mr0Challenge::createMr0Showcase, BaroqueWorksChallenge::createMr1Showcase, BaroqueWorksChallenge::createMr2Showcase, BaroqueWorksChallenge::createMr3Showcase).setTimeLimit(30).setRewards(REWARD).build();
 
     public BaroqueWorksHardChallenge(ChallengeCore core) {
         super(core);
-    }
-
-    private static LivingEntity createMr1Showcase(World world) {
-        return ModEntities.MR1.get().create(world);
     }
 }
