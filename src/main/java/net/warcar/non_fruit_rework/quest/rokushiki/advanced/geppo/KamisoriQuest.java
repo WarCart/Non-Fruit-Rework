47d96e7d8f45a281a899620918f8d02b16bdf49e
Warcar <@imasanov2007@gmail,com>
2025-05-17 11:39:12 +0300

    Fixed survive quests

--- src/main/java/net/warcar/non_fruit_rework/quest/rokushiki/advanced/geppo/KamisoriQuest.java
+++ src/main/java/net/warcar/non_fruit_rework/quest/rokushiki/advanced/geppo/KamisoriQuest.java
@@ -3,6 +3,7 @@ package net.warcar.non_fruit_rework.quest.rokushiki.advanced.geppo;
 import net.minecraft.entity.LivingEntity;
 import net.warcar.non_fruit_rework.helpers.IHasRequirements;
 import net.warcar.non_fruit_rework.helpers.QuestHelper;
+import net.warcar.non_fruit_rework.quest.objectives.CustomSurvivalObjective;
 import net.warcar.non_fruit_rework.quest.objectives.CustomUseAbilityObjective;
 import net.warcar.non_fruit_rework.quest.objectives.RunObjective;
 import net.warcar.non_fruit_rework.quest.rokushiki.GeppoQuest;
@@ -12,7 +13,6 @@ import xyz.pixelatedw.mineminenomi.abilities.rokushiki.SoruAbility;
 import xyz.pixelatedw.mineminenomi.api.quests.Quest;
 import xyz.pixelatedw.mineminenomi.api.quests.QuestId;
 import xyz.pixelatedw.mineminenomi.quests.objectives.ReachDorikiObjective;
-import xyz.pixelatedw.mineminenomi.quests.objectives.TimedSurvivalObjective;
 
 public class KamisoriQuest extends Quest implements IHasRequirements {
     public static final QuestId<KamisoriQuest> INSTANCE = new QuestId.Builder<>("Trial: Kamisori", KamisoriQuest::new)
@@ -22,7 +22,7 @@ public class KamisoriQuest extends Quest implements IHasRequirements {
         super(core);
         ReachDorikiObjective objective = new ReachDorikiObjective("Get %s Doriki Strong", 1000);
         this.addObjective(objective);
-        this.addObjective(new TimedSurvivalObjective("Survive for %s seconds without getting hit", 300).addRequirement(objective));
+        this.addObjective(new CustomSurvivalObjective(300).addRequirement(objective));
         this.addObjective(new RunObjective(1200).addRequirement(objective));
         this.addObjective(new CustomUseAbilityObjective(50, SoruAbility.INSTANCE).addRequirement(objective));
         this.addObjective(new CustomUseAbilityObjective(60, GeppoAbility.INSTANCE).addRequirement(objective));
